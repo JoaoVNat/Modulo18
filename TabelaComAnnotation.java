990b4f4f48cbf2251375126ad840b6b23fef8eb9
JoaoVNat <joaovictor567bmsk8@hotmail.com>
2023-09-27 19:08:04 -0300

    first commit

---
+++ TabelaComAnnotation.java
@@ -0,0 +1,7 @@
+@TabelaAnnotation(value = "Tabela Com Classes", sala = "Terceiro Ano A", numeroDaSala = 2)
+public class TabelaComAnnotation {
+
+    @TabelaAnnotation(value = "Tabela Com Classes", sala = "Terceiro Ano B", numeroDaSala = 3)
+    private String salas;    
+
+}
